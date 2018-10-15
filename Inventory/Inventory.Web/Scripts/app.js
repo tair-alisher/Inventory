7c19edd936a9aec50736d0def2440266423675fc
Alisher <ur.alisher@mail.ru>
2018-10-15 07:41:01 +0600

    equipment create view updated

---
+++ Inventory/Inventory.Web/Scripts/app.js
@@ -0,0 +1,20 @@
+﻿function searchEmployees() {
+    var employeeName = document.getElementById('search-input-value').value;
+    var token = $('input[name="__RequestVerificationToken"]').val();
+
+    $.ajax({
+        url: "/Equipments/FindEmployee",
+        type: "Post",
+        data: {
+            __RequestVerificationToken: token,
+            "name": employeeName
+        },
+        success: function (data) {
+
+        },
+        error: function (XMLHttpRequest) {
+            console.log(XMLHttpRequest);
+        }
+    });
+    return false;
+}
