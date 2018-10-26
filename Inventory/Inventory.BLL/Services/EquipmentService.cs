fa02fe96255b6477e061ec08ff38dd7ecd3310b9
VFedyaev <VFedyaev@stat.kg>
2018-10-26 17:09:46 +0600

    generate qrCode

--- Inventory/Inventory.BLL/Services/EquipmentService.cs
+++ Inventory/Inventory.BLL/Services/EquipmentService.cs
@@ -6,6 +6,13 @@ using Inventory.BLL.Interfaces;
 using Inventory.DAL.Entities;
 using Inventory.DAL.Interfaces;
 using Inventory.BLL.Infrastructure;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZXing;
 
 namespace Inventory.BLL.Services
 {
@@ -27,11 +34,42 @@ namespace Inventory.BLL.Services
             Equipment equipment = BLLEquipmentMapper.DtoToEntity(equipmentDTO);
             equipment.Id = Guid.NewGuid();
 
+            string url = $"http://localhost:58644/Equipment/Details/{equipment.Id}";
+            equipment.QRCode = GenerateQRCode(url, equipment.Id);
+
             _unitOfWork.Equipments.Create(equipment);
             _unitOfWork.Save();
 
             return equipment.Id;
         }
+        private string GenerateQRCode(string qrcodeText, Guid id)
+        {
+            string folderPath = "~/Content/Images/";
+            string imagePath = $"/Content/Images/{id}.jpg";
+            // If the directory doesn't exist then create it.
+            if (!Directory.Exists(HttpContext.Current.Server.MapPath(folderPath)))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            var barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+            var result = barcodeWriter.Write(qrcodeText);
+
+            string barcodePath = HttpContext.Current.Server.MapPath(imagePath);
+            var barcodeBitmap = new Bitmap(result);
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(barcodePath, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    barcodeBitmap.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes = memory.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+            return imagePath;
+        }
+
 
         public EquipmentDTO Get(Guid id)
         {
