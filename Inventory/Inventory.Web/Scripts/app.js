6bd184f3dc7850e928d6bf241bbde01c3813e953
VFedyaev <VFedyaev@stat.kg>
2018-11-12 14:59:23 +0600

    print button

--- Inventory/Inventory.Web/Scripts/app.js
+++ Inventory/Inventory.Web/Scripts/app.js
@@ -302,3 +302,18 @@ function toPrevMain(from = "") {
 function closeMessageDiv() {
     $("#message-div").remove();
 }
+
+function QRCodeSourcetoPrint(source) {
+    return "<html><head><script>function step1(){\n" +
+        "setTimeout('step2()', 10);}\n" +
+        "function step2(){window.print();window.close()}\n" +
+        "</scri" + "pt></head><body onload='step1()'>\n" +
+        "<img src='" + source + "' /></body></html>";
+}
+function QRCodePrint(source) {
+    Pagelink = "about:blank";
+    var pwa = window.open(Pagelink, "_new");
+    pwa.document.open();
+    pwa.document.write(QRCodeSourcetoPrint(source));
+    pwa.document.close();
+}
