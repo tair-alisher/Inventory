9239407640299e088323ba4e822c8c681c44dffe
VFedyaev <VFedyaev@stat.kg>
2018-11-13 15:32:51 +0600

    not working filters

--- Inventory/Inventory.Web/Controllers/HistoryController.cs
+++ Inventory/Inventory.Web/Controllers/HistoryController.cs
@@ -41,7 +41,7 @@ namespace Inventory.Web.Controllers
             return PartialView(historyVMs.ToPagedList(pageNumber, pageSize));
         }
 
-        public ActionResult Index(int? page)
+        public ActionResult Index(int? page, string repairPlaceId, string statusId)
         {
             int pageSize = 10;
             int pageNumber = (page ?? 1);
@@ -68,14 +68,7 @@ namespace Inventory.Web.Controllers
 
             ViewBag.EquipmentId = equipmentSelectModel;
 
-            //List<CatalogEntities.Employee> employees = EmployeeService.GetAll().ToList();
-
             ViewBag.EmployeeId = EmployeeService.GetAll().ToList(); 
-
-            //ViewBag.EmployeeId = new SelectList(
-            //    EmployeeService.GetAll(),
-            //    "EmployeeId",
-            //    "EmployeeFullName");
 
             IEnumerable<HistoryDTO> historyDTOs = HistoryService.GetAll().ToList();
 
